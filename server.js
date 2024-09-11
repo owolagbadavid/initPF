c2aaeff7b3b6d12edb4f20ec4e86ac3aa28493a8
DAVID <67152500+owolagbadavid@users.noreply.github.com>
2024-09-11 14:59:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const port = process.env.PORT || 4000;
 const canonicalDomain1 = process.env.CAN1;
 const canonicalDomain2 = process.env.CAN2;
 
-app.use((req, res, next) => {
-  // Check if the host matches the canonical domain
-  console.log(req.headers.host, canonicalDomain1, canonicalDomain2, `https://${canonicalDomain1}${req.url}`)
-  if (req.headers.host !== canonicalDomain1 || req.headers.host !== canonicalDomain2) {
-    // Redirect to the canonical domain using a permanent redirect (301)
-    res.redirect(301, `https://${canonicalDomain1}${req.url}`);
-  } else {
-    next();
-  }
-});
+// app.use((req, res, next) => {
+//   // Check if the host matches the canonical domain
+//   console.log(req.headers.host, canonicalDomain1, canonicalDomain2, `https://${canonicalDomain1}${req.url}`)
+//   if (req.headers.host !== canonicalDomain1 || req.headers.host !== canonicalDomain2) {
+//     // Redirect to the canonical domain using a permanent redirect (301)
+//     res.redirect(301, `https://${canonicalDomain1}${req.url}`);
+//   } else {
+//     next();
+//   }
+// });
 
 app.set("view engine", "ejs");
 

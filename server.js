3394a99bd90b91c1ca5b5438e4d2d05a19f9540c
DAVID <67152500+owolagbadavid@users.noreply.github.com>
2024-09-11 14:33:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 
+const canonicalDomain1 = process.env.CAN1;
+const canonicalDomain2 = process.env.CAN2;
+
+app.use((req, res, next) => {
+  // Check if the host matches the canonical domain
+  if (req.headers.host !== canonicalDomain1 || req.headers.host !== canonicalDomain2) {
+    // Redirect to the canonical domain using a permanent redirect (301)
+    res.redirect(301, `https://${canonicalDomain1}${req.url}`);
+  } else {
+    next();
+  }
+});
+
 app.set("view engine", "ejs");
 
 app.use("/static", express.static(path.join(__dirname, "public")));
